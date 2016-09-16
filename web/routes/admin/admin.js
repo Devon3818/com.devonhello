bf113f1b0549164d1b8c18df4a078d81363395f4
Devon <k849996781@vip.qq.com>
2016-09-16 12:01:27 +0800

    2016-09-16

--- web/routes/admin/admin.js
+++ web/routes/admin/admin.js
@@ -2,9 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+//后台登录页面
 router.get('/', function(req, res, next) {
   //res.send('Admin');
-  res.render('admin', { title: 'Express' });
+  res.render('Admin/admin', { title: 'Express' });
+});
+
+
+//后台主页面
+router.get('/index', function(req, res, next) {
+  //res.send('Admin');
+  res.render('Admin/index', { title: 'Express' });
+});
+
+router.get('/index/f1', function(req, res, next) {
+  //res.send('Admin');
+  res.render('Admin/frames/frame1', { title: 'Express' });
 });
 
 module.exports = router;
